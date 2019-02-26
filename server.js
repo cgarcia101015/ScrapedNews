ffcb3a5050a1e6548f4eb986e8fad1804b22a20c
Carlos M Garcia <cgarcia101015@gmail.com>
2019-02-25 22:31:22 -0500

    got articles to save and clear button to delete articles

--- server.js
+++ server.js
@@ -84,6 +84,38 @@ app.get('/articles', function(req, res) {
 		});
 });
 
+// Route for displaying saved articles
+app.get('/saved', function(req, res) {
+	// Grab every document in the Articles collection
+	db.Article
+		.find({ saved: true })
+		.then(function(dbArticle) {
+			// If we were able to successfully find Articles, send them back to the client
+			res.json(dbArticle);
+		})
+		.catch(function(err) {
+			// If an error occurred, send it to the client
+			res.json(err);
+		});
+});
+
+// Route for clearing all Articles from the db
+app.get('/api/clear', function(req, res) {
+	// Grab every document in the Articles collection
+	db.Article
+		.find(req.query)
+		.then(function(dbArticle) {
+			// If we were able to successfully find Articles, send them back to the client
+			db.Article.collection.deleteMany();
+			initPage();
+			// res.json(dbArticle);
+		})
+		.catch(function(err) {
+			// If an error occurred, send it to the client
+			res.json(err);
+		});
+});
+
 // Route to save Articles
 app.put('/articles/:id', function(req, res) {
 	db.Article.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true }).then(function(savedArticle) {
