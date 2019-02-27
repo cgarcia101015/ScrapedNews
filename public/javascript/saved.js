c18cb830ba01a97e8695c10a65008b7d8c3a7006
Carlos M Garcia <cgarcia101015@gmail.com>
2019-02-26 21:29:09 -0500

    working on notes functionality

--- public/javascript/saved.js
+++ public/javascript/saved.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
 	// and "scrape new article" buttons
 	var articleContainer = $('.article-container');
 	$(document).on('click', '.btn.delete', removeFromSaved);
-	// $(document).on('click', '.btn.notes');
+	$(document).on('click', '.btn.notes', handleNotes);
+	$(document).on('click', '.btn.save', handleNoteSave);
+	$(document).on('click', '.btn.note-delete', handleNoteDelete);
 
 	$('.clear').on('click', handleArticleClear);
 	console.log('hello world');
@@ -21,6 +23,96 @@ $(document).ready(function() {
 				renderEmpty();
 			}
 		});
+	}
+
+	function handleNotes(event) {
+		// This function handles opening the notes modal and displaying our notes
+		// We grab the id of the article to get notes for from the card element the delete button sits inside
+		var currentArticle = $(this).parents('.card').data();
+		// Grab any notes with this headline/article id
+		$.get('/api/notes/' + currentArticle._id).then(function(data) {
+			// Constructing our initial HTML to add to the notes modal
+			var modalText = $("<div class='container-fluid text-center'>").append(
+				$('<h4>').text('Notes For Article: ' + currentArticle._id),
+				$('<hr>'),
+				$("<ul class='list-group note-container'>"),
+				$("<textarea placeholder='New Note' rows='4' cols='60'>"),
+				$("<button class='btn btn-success save'>Save Note</button>")
+			);
+			// Adding the formatted HTML to the note modal
+			bootbox.dialog({
+				message: modalText,
+				closeButton: true
+			});
+			var noteData = {
+				_id: currentArticle._id,
+				notes: data || []
+			};
+			// Adding some information about the article and article notes to the save button for easy access
+			// When trying to add a new note
+			$('.btn.save').data('article', noteData);
+			// renderNotesList will populate the actual note HTML inside of the modal we just created/opened
+			renderNotesList(noteData);
+		});
+	}
+
+	function handleNoteSave() {
+		// This function handles what happens when a user tries to save a new note for an article
+		// Setting a variable to hold some formatted data about our note,
+		// grabbing the note typed into the input box
+		var noteData;
+		var newNote = $('.bootbox-body textarea').val().trim();
+		// If we actually have data typed into the note input field, format it
+		// and post it to the "/api/notes" route and send the formatted noteData as well
+		if (newNote) {
+			noteData = { _headlineId: $(this).data('article')._id, noteText: newNote };
+			$.post('/api/notes', noteData).then(function() {
+				// When complete, close the modal
+				bootbox.hideAll();
+			});
+		}
+	}
+
+	function handleNoteDelete() {
+		// This function handles the deletion of notes
+		// First we grab the id of the note we want to delete
+		// We stored this data on the delete button when we created it
+		var noteToDelete = $(this).data('_id');
+		// Perform an DELETE request to "/api/notes/" with the id of the note we're deleting as a parameter
+		$.ajax({
+			url: '/api/notes/' + noteToDelete,
+			method: 'DELETE'
+		}).then(function() {
+			// When done, hide the modal
+			bootbox.hideAll();
+		});
+	}
+
+	function renderNotesList(data) {
+		// This function handles rendering note list items to our notes modal
+		// Setting up an array of notes to render after finished
+		// Also setting up a currentNote variable to temporarily store each note
+		var notesToRender = [];
+		var currentNote;
+		if (!data.notes.length) {
+			// If we have no notes, just display a message explaining this
+			currentNote = $("<li class='list-group-item'>No notes for this article yet.</li>");
+			notesToRender.push(currentNote);
+		} else {
+			// If we do have notes, go through each one
+			for (var i = 0; i < data.notes.length; i++) {
+				// Constructs an li element to contain our noteText and a delete button
+				currentNote = $("<li class='list-group-item note'>")
+					.text(data.notes[i].noteText)
+					.append($("<button class='btn btn-danger note-delete'>x</button>"));
+				// Store the note id on the delete button for easy access when trying to delete
+				currentNote.children('button').data('_id', data.notes[i]._id);
+				// Adding our currentNote to the notesToRender array
+				notesToRender.push(currentNote);
+			}
+		}
+		// Now append the notesToRender to the note-container inside the note modal
+		$('.note-container').append(notesToRender);
 	}
 
 	function renderArticles(articles) {
--- server.js
+++ server.js
@@ -132,8 +132,9 @@ app.post('/api/notes', function(req, res) {
 });
 
 app.get('/api/notes', function(req, res) {
-	db.Note.find({ _headlineId: req.body._headlineId }).then(function(dbNotes) {
-		res.json(dbNotes);
+	db.Note.find({ _headlineId: req.body._headlineId }).then(function(dbNote) {
+		console.log('noted');
+		res.json(dbNote);
 	});
 });
 
@@ -178,26 +179,26 @@ app.get('/articles/:id', function(req, res) {
 		});
 });
 
-// Route for saving/updating an Article's associated Note
-app.post('/articles/:id', function(req, res) {
-	// Create a new note and pass the req.body to the entry
-	db.Note
-		.create(req.body)
-		.then(function(dbNote) {
-			// If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-			// { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-			// Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-			return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-		})
-		.then(function(dbArticle) {
-			// If we were able to successfully update an Article, send it back to the client
-			res.json(dbArticle);
-		})
-		.catch(function(err) {
-			// If an error occurred, send it to the client
-			res.json(err);
-		});
-});
+// // Route for saving/updating an Article's associated Note
+// app.post('/articles/:id', function(req, res) {
+// 	// Create a new note and pass the req.body to the entry
+// 	db.Note
+// 		.create(req.body)
+// 		.then(function(dbNote) {
+// 			// If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+// 			// { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+// 			// Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+// 			return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+// 		})
+// 		.then(function(dbArticle) {
+// 			// If we were able to successfully update an Article, send it back to the client
+// 			res.json(dbArticle);
+// 		})
+// 		.catch(function(err) {
+// 			// If an error occurred, send it to the client
+// 			res.json(err);
+// 		});
+// });
 
 // Start the server
 app.listen(PORT, function() {
